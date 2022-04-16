355f607d80570af3cdd2d525b3a5c034ec94828d
Jordan anderson <mr@jordananderson.io>
2022-04-16 03:03:34 +0100

    remove script to install credential helper

--- build/Tasks/PackSolution.cs
+++ build/Tasks/PackSolution.cs
@@ -19,17 +19,7 @@ public sealed class PackSolution : AsyncFrostingTask<BuildContext>
   public override async Task RunAsync(BuildContext context)
   {
     Environment.SetEnvironmentVariable("", $"{{\"endpointCredentials\": [{{\"endpoint\":\"{context.Parameters[BuildContext.FeedUrl]}\", \"username\":\"docker\", \"password\":\"{context.Parameters[BuildContext.FeedAccessToken]}\"}}]}}");
-    var result = await Cli.Wrap("curl")
-      .WithArguments(new[]
-      {
-        "-L",
-        "https://raw.githubusercontent.com/Microsoft/artifacts-credprovider/master/helpers/installcredprovider.sh",
-        $"| sh"
-      }, false)
-      .ExecuteBufferedAsync();
-
-    Console.WriteLine(result.StandardError + result.StandardOutput);
-
+    
     context.DotNetPack($"../../{context.Parameters[BuildContext.ProjectName]}.sln", new DotNetPackSettings
     {
       Configuration = "Release",

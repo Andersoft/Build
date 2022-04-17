bec5f9b700b680a412695842f8d3f71f91ad35ec
Jordan anderson <mr@jordananderson.io>
2022-04-17 01:35:21 +0100

    skip build if version already exists

--- build/Tasks/DockerBuild.cs
+++ build/Tasks/DockerBuild.cs
@@ -1,5 +1,7 @@
+using System.Threading.Tasks;
 using Cake.Docker;
 using Cake.Frosting;
+using CliWrap.Buffered;
 
 [TaskName("Docker Build")]
 [IsDependentOn(typeof(DockerAuthenticate))]
@@ -7,12 +9,24 @@ public sealed class DockerBuild : FrostingTask<BuildContext>
 {
   public override bool ShouldRun(BuildContext context)
   {
-    return context.IsValid;
+    var projectName = context.Parameters[BuildContext.ProjectName].ToLower();
+    var response = CliWrap.Cli.Wrap("docker")
+      .WithArguments(new[]
+      {
+        "manifest",
+        "inspect",
+        $"{context.Parameters[BuildContext.DockerRegistry]}.azurecr.io/{projectName}:{context.Parameters[BuildContext.BuildVersion]}"
+      })
+      .ExecuteBufferedAsync()
+      .GetAwaiter();
+    var result = response.GetResult();
+
+    return context.IsValid && (result.StandardError + result.StandardOutput).Contains("no such manifest") is not true;
   }
   public override void Run(BuildContext context)
   {
     var projectName = context.Parameters[BuildContext.ProjectName].ToLower();
-
+    
     context.DockerBuild(new DockerImageBuildSettings
     {
       BuildArg = new[]

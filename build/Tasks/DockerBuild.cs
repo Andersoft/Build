26675a754dde66470bed4b0ae16ea1065d3b5e80
Jordan anderson <mr@jordananderson.io>
2022-04-17 01:57:17 +0100

    should build

--- build/Tasks/DockerBuild.cs
+++ build/Tasks/DockerBuild.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Cake.Docker;
 using Cake.Frosting;
@@ -10,18 +11,26 @@ public sealed class DockerBuild : FrostingTask<BuildContext>
   public override bool ShouldRun(BuildContext context)
   {
     var projectName = context.Parameters[BuildContext.ProjectName].ToLower();
-    var response = CliWrap.Cli.Wrap("docker")
-      .WithArguments(new[]
-      {
-        "manifest",
-        "inspect",
-        $"{context.Parameters[BuildContext.DockerRegistry]}.azurecr.io/{projectName}:{context.Parameters[BuildContext.BuildVersion]}"
-      })
-      .ExecuteBufferedAsync()
-      .GetAwaiter();
-    var result = response.GetResult();
+    bool shouldBuild = false;
+    try
+    {
+      var response = CliWrap.Cli.Wrap("docker")
+        .WithArguments(new[]
+        {
+          "manifest",
+          "inspect",
+          $"{context.Parameters[BuildContext.DockerRegistry]}.azurecr.io/{projectName}:{context.Parameters[BuildContext.BuildVersion]}"
+        })
+        .ExecuteBufferedAsync()
+        .GetAwaiter();
+      var result = response.GetResult();
+    }
+    catch (Exception e)
+    {
+      shouldBuild = true;
+    }
 
-    return context.IsValid && (result.StandardError + result.StandardOutput).Contains("no such manifest") is not true;
+    return context.IsValid && shouldBuild;
   }
   public override void Run(BuildContext context)
   {
